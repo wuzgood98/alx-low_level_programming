561e34ba57c576aaffa6919b86da3812620c5799
Gideon Addo <gideonaddoofori@gmail.com>
2023-06-15 22:57:32 +0000

    Add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * Program that uses puts function
+ * to print a string
+ * and always returns 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
