a123e517226afe04d45835096cd883cf19b5d695
Gideon Addo <gideonaddoofori@gmail.com>
2023-06-15 23:17:24 +0000

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main: entry point
+ * main - Entry point
  *
  * Return: always return 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
