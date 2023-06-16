51c98c41bf32f732058fcd66dac18dc7a9831d94
Gideon Addo <gideonaddoofori@gmail.com>
2023-06-16 23:42:23 +0000

    Add 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - print all the numbers of base 16 in lowercase, followed by a new line.
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	int i;
+
+	// Print numbers of base 16 in lowercase
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+			// Convert to character using ASCII value
+			putchar('0' + i);
+		}
+		else
+		{
+			// Convert to lowercae hexadecimal character
+			putchar('a' + (i - 10));
+		}
+	}
+	putchar('\n');
+	return (0);
+}
