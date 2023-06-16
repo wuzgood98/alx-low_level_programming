05e5d5ef1c511fb0f13a3f5245502400e0fdff01
Gideon Addo <gideonaddoofori@gmail.com>
2023-06-16 23:09:42 +0000

    Add 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print all alphabets except q and e.
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'q' || i == 'e')
+		{
+			continue;
+		}
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
