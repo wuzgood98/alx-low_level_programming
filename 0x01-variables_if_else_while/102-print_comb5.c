a0ba20ed43a77c29f9c0b19752a95c88d296545b
Gideon Addo <gideonaddoofori@gmail.com>
2023-06-17 01:05:36 +0000

    Add 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Print all possible combinations of two two-digit
+ * numbers
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i; j <= 99; j++)
+		{
+			/* Print the tens digit of the first number */
+			putchar('0' + i / 10);
+			/* Print the ones digit of the first number */
+			putchar('0' + i % 10);
+			/* Print the space separator */
+			putchar(' ');
+			/* Print the tens digit of the second number */
+			putchar('0' + j / 10);
+			/* Print the ones digit of the second number */
+			putchar('0' + j % 10);
+			if (i != 99 || j != 99)
+			{
+				/* Print the comma separator */
+				putchar(',');
+				/* Print the space separator */
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
