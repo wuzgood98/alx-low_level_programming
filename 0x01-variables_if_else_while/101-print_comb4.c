aee877b88dea36335ab924e96603996fd263e28f
Gideon Addo <gideonaddoofori@gmail.com>
2023-06-17 00:49:04 +0000

    Add 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Print all possible different combinations
+ * of three digits
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				/* Print the first digit */
+				puthcar('O' + i);
+				/* Print the second digit */
+				putchar('O' + j);
+				/* Print the third digit */
+				putchar('O' + k);
+				if (i < 7 || j < 8 || k < 9)
+				{
+					/* Print the comma separator */
+					putchar(',');
+					/* Print the space separator */
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
