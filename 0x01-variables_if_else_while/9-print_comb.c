eeb18f4956ebf0b7a0382baf65ec3868f583cedb
Gideon Addo <gideonaddoofori@gmail.com>
2023-06-17 00:17:48 +0000

    Add 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combinations of single-digit
+ * numbers.
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	int dgt;
+
+	for (dgt = 48; dgt <= 57; dgt++)
+	{
+		putchar(dgt);
+		if (dgt == 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
