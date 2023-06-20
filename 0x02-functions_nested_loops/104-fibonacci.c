ffbc30801fdab58db695df20cf2794cbacefc2aa
Gideon Addo <gideonaddoofori@gmail.com>
2023-06-20 23:37:35 +0000

    Add 104-fibonnaci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - print first 98 fibonacci numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, a, b, c;
+
+	a = 1;
+	b = 2;
+
+	printf("%d, %d", a, b);
+
+	for (i = 3; i <= 98; i++)
+	{
+		c = a + b;
+		printf(", %d", c);
+
+		a = b;
+		b = c;
+	}
+
+	printf("\n");
+}
