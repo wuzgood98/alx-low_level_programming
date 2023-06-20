c36256d979c75199123b5a7367bb73282697d819
Gideon Addo <gideonaddoofori@gmail.com>
2023-06-20 18:31:01 +0000

    Add 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Program that prints _putchar,
+ * followed by a new line
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int char[8] = {95,112,117,116,99,104,97,114};
+	int i, sz;
+
+	sz = sizeof(char) / sizeof(char[0]);
+	for (i = 0; i < sz; i++)
+	{
+		putchar(char[i]);
+	}
+
+	putchar('\n');
+	return (0);
+}
