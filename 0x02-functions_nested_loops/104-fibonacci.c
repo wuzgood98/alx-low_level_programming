a3924b55be637664483e22e979f5bf2e0e0eec86
Gideon Addo <gideonaddoofori@gmail.com>
2023-06-21 00:11:27 +0000

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,23 +6,44 @@
  */
 int main(void)
 {
-	int i;
-	long int a, b, c;
+	int c;
+	unsigned long fb1 = 0, fb2 = 1, sum;
+	unsigned long fb1_1, fb1_2, fb2_1, fb2_2;
+	unsigned long h1, h2;
 
-	a = 1;
-	b = 2;
-
-	printf("%ld, %ld", a, b);
-
-	for (i = 3; i <= 98; i++)
+	for (c = 0; c < 92; c++)
 	{
-		c = a + b;
-		printf(", %ld", c);
+		sum = fb1 + fb2;
+		printf("%lu, ", sum);
 
-		a = b;
-		b = c;
+		fb1 = fb2;
+		fb2 = sum;
 	}
 
+	fb1_1 = fb1 / 10000000000;
+	fb2_1 = fb2 / 10000000000;
+	fb1_2 = fb1 % 10000000000;
+	fb2_2 = fb2 % 10000000000;
+
+	for (c = 93; c < 99; c++)
+	{
+		h1 = fb1_1 + fb2_1;
+		h2 = fb1_2 + fb2_2;
+		if (fb1_2 + fb2_2 > 9999999999)
+		{
+			h1 += 1;
+			h2 %= 10000000000;
+		}
+
+		printf("%lu%lu", h1, h2);
+		if (c != 98)
+			printf(", ");
+
+		fb1_1 = fb2_1;
+		fb1_2 = fb2_2;
+		fb2_1 = h1;
+		fb2_2 = h2;
+	}
 	printf("\n");
 	return (0);
 }
