f30392199aac311aee11a1cdf2af395bc4618d2e
Gideon Addo <gideonaddoofori@gmail.com>
2023-06-22 23:28:02 +0000

    Add 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ *
+ * @size: size of the triangle
+ *
+ * Return: returns nothing
+ */
+
+void print_triangle(int size)
+{
+	int hgt, wdt;
+
+	if (size > 0)
+	{
+		for (hgt = 0; hgt < size; hgt++)
+		{
+			for (wdt = size - hgt; wdt > 0; wdt--)
+			{
+				_putchar(' ');
+			}
+
+			for (wdt = 0; wdt < hgt; wdt++)
+			{
+				_putchar('#');
+			}
+
+			for (hgt == size)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
