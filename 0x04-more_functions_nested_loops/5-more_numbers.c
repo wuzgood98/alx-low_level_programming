488ce327aec57c40604661366038f5084b090ca2
Gideon Addo <gideonaddoofori@gmail.com>
2023-06-22 22:01:06 +0000

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,17 +9,17 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int num, count;
 
-	for (i = 0; i < 10; i++)
+	for (count = 0; count < 10; count++)
 	{
-		for (j = 0; j < 15; j++)
+		for (num = 0; num < 15; num++)
 		{
-			if (j >= 10)
+			if (num >= 10)
 			{
-				_putchar((j / 10) + 48);
+				_putchar((num / 10) + '0');
 			}
-			_putchar((j % 10) + 48);
+			_putchar((num % 10) + '0');
 		}
 	}
 	_putchar('\n');
