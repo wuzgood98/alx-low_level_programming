bf22b2ca816c0ac4452a9e6c28347933ecf42992
Gideon Addo <gideonaddoofori@gmail.com>
2023-06-26 18:23:54 +0000

    Add 9-strcp.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies value from a variable to the other
+ * @dest: variable to copy to
+ * @src: variable to copy from
+ *
+ * Return: value copied
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int length = 0;
+
+	while (length >= 0)
+	{
+		*dest[length] = *src[length];
+		if (*src[length] == '\0')
+			break;
+		length++;
+	}
+	return (dest);
+}
