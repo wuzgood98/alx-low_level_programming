d55bb715eef62a90e1c15b7db0b87a493949afb3
Gideon Addo <gideonaddoofori@gmail.com>
2023-06-26 16:17:08 +0000

    Add 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ *
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int strlen = 0;
+	int i;
+	char revd_str;
+
+	while (strlen >= 0)
+	{
+		if (s[strlen] == '\0')
+			break;
+		strlen++;
+	}
+
+	for (strlen--; strlen >= 0; strlen--)
+	{
+		revd_str += s[strlen];
+		*s = revd_str;
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
