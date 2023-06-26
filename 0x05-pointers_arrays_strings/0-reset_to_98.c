9ca84b4950cb26c203ab5f7e6737e8a252125478
Gideon Addo <gideonaddoofori@gmail.com>
2023-06-26 14:58:41 +0000

    Add 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Update the value of a pointer to 98
+ * @n: pointer to an int
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
