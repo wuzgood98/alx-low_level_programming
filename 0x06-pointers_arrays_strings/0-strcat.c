7f0642e7fb00a73fb18267552a91d4dc2b83afe7
Gideon Addo <gideonaddoofori@gmail.com>
2023-06-29 11:30:11 +0000

    Refactor

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,27 +10,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int destlen = 0, srclen = 0, i;
+	int destlen = 0, srclen = 0;
 
-	while (destlen >= 0)
-	{
-		if (dest[destlen] == '\0')
-			break;
+	while (*(dest + destlen) != '\0')
 		destlen++;
-	}
 
 	while (srclen >= 0)
 	{
-		if (src[srclen] == '\0')
+		*(dest + destlen) = *(src + srclen);
+		if (*(src + srclen) == '\0')
 			break;
 		srclen++;
+		destlen++;
 	}
-
-	for (i = 0; i < srclen; i++)
-	{
-		dest[destlen + i] = src[i];
-	}
-	dest[destlen + i] = '\0';
 
 	return (dest);
 }
