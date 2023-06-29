4d3a7f536bd8a68408ba4c883bfa419018a7dfbd
Gideon Addo <gideonaddoofori@gmail.com>
2023-06-29 13:05:43 +0000

    Fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,14 +11,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, operation = 0;
+	int i = 0;
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
-			operation = s1[i] - s2[i];
+			return (s1[i] - s2[i]);
 		i++;
 	}
 
-	return (operation);
+	return (s1[i] - s2[i]);
 }
