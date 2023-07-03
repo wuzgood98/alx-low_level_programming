967163d4a5537233614e6b8696ca9c47eb0962dc
Gideon Addo <gideonaddoofori@gmail.com>
2023-07-03 18:02:03 +0000

    Fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,12 +15,11 @@ char *_strstr(char *haystack, char *needle)
 
 	while (*haystack)
 	{
-		while (haystack[i] == needle[i])
-		{
+		do {
 			if (needle[i + 1] == '\0')
 				return (haystack);
 			i++;
-		}
+		} while (haystack[i] == needle[i]);
 		haystack++;
 	}
 	return ('\0');
