67295652d3ede82b57a3d0fd4e8bfb57cb6d6076
Gideon Addo <gideonaddoofori@gmail.com>
2023-07-05 15:57:18 +0000

    [Fix]

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,42 +3,20 @@
 /**
  * sqrt_helper - returns the natural root of a number
  * @n: base number
- * @start: start of iterator
- * @end: end of iterator
+ * @sqrt: root
  *
  * Return: square root or -1
  */
 
-int sqrt_helper(int n, int start, int end)
+int sqrt_helper(int n, int sqrt)
 {
-	int mid;
+	if ((sqrt * sqrt) == n)
+		return (sqrt);
 
-	/**
-	 * Base case: If the start value exceeds the end value,
-	 * the square root is not found
-	 */
-	if (start > end)
+	if (sqrt == (n / 2))
 		return (-1);
 
-	/* Find the mid value between the start and end values */
-	mid = (start + end) / 2;
-
-	/**
-	 * Base case: If the square of the mid is equal to n,
-	 * return mid as the square root.
-	 */
-	if ((mid * mid) == n)
-		return (mid);
-
-	/**
-	 * If the square of the mid is greater than n,
-	 * search in the lower half
-	 */
-	if ((mid * mid) > n)
-		return (sqrt_helper(n, start, mid));
-	/* If the sqaure of mid is less than n, search in the upper half */
-	else
-		return (sqrt_helper(n, mid + 1, end));
+	return (sqrt_helper(n, sqrt + 1));
 }
 
 /**
@@ -51,13 +29,13 @@ int sqrt_helper(int n, int start, int end)
 
 int _sqrt_recursion(int n)
 {
+	int sqrt = 0;
 	/* Handle the case of negative input */
 	if (n < 0)
 		return (-1);
 
-	/*
-	 * Call the herlper function to find the square
-	 * root within the range of 0 to n
-	 */
-	return (sqrt_helper(n, 0, n));
+	if (n == 1)
+		return (1);
+
+	return (sqrt_helper(n, sqrt));
 }
