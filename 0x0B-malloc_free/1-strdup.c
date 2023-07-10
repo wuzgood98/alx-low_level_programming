e7aee088a43e9e3c4fc286b948942f996b5511b4
Gideon Addo <gideonaddoofori@gmail.com>
2023-07-10 23:48:07 +0000

    Add 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - finds the length of a string.
+ * @str: string.
+ *
+ * Return: length of str.
+ */
+
+int _strlen(char *str)
+{
+	if (*str == '\0')
+		return (0);
+	return (1 + _strlen(s + 1));
+}
+
+/**
+ * _strdup - a pointer to the newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: parameter as string.
+ *
+ * Return: NULL is str is NULL or pointer to the newly allocated memory
+ */
+
+char *_strdup(char *str)
+{
+	char *new_str;
+	unsigned int i = 0;
+	int length;
+
+	if (str == NULL)
+		return (NULL);
+
+	length = _strlen(str);
+	new_str = malloc(sizeof(str) * length);
+
+	if (new_str == NULL)
+		return (NULL);
+
+	while (i < length)
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+
+	new_str[length] = '\0';
+
+	return (new_str);
+}
