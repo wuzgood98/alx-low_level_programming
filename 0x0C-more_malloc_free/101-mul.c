498efde90b6dbf66b1f59c1b75293709a516c458
Gideon Addo <gideonaddoofori@gmail.com>
2023-07-13 22:26:47 +0000

    Add 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,223 +1,103 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-int _strlen(char *s);
-char *init_array(int size);
-char *iterate(char *str);
-void product(char *prod, char *mult, int digit, int zeroes);
-void add_digits(char *prod1, char *prod2, int len);
+#include "main.h"
 
 /**
- * _strlen - Finds the length of a string.
- * @s: The string to be measured.
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: length of the string.
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
+
+int is_digit(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - finds the length of a string
+ * @s: string to evaluate
+ *
+ * Return: length of the string
+ */
+
 int _strlen(char *s)
 {
-	if (*s == '\0')
-		return (0);
+	int i = 0;
 
-	return (1 + _strlen(s + 1));
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
 /**
- * init_array - Creates an array of chars and initializes it with
- * the character 'x'.
- * @size: size of the array.
- *
- * Return: pointer to the array.
+ * handle_error - handles errors for main
  */
-char *init_array(int size)
+
+void handle_error(void)
 {
-	char *array;
-	int i;
-
-	array = malloc(sizeof(char) * size);
-
-	if (array == NULL)
-		exit(98);
-
-	for (i = 0; i < (size - 1); i++)
-		array[i] = 'x';
-
-	array[i] = '\0';
-
-	return (array);
+	printf("Error\n");
+	exit(98);
 }
 
 /**
- * iterate - iterates through a string of numbers containing
- * leading zeroes until it hits a non-zero number.
- * @str: the string of numbers to be iterated.
+ * main - multiplies two numbers
+ * @argc: arguments count
+ * @argv: pointer to the arguments
  *
- * Return: pointer to the next non-zero element.
+ * Return: always 0.
  */
-char *iterate(char *str)
-{
-	while (*str && *str == '0')
-		str++;
 
-	return (str);
-}
-
-/**
- * to_int - converts a digit character to a corresponding int.
- * @c: The chara to be converted.
- *
- * Return: the converted digit.
- */
-int to_int(char c)
-{
-	int digit = c - '0';
-
-	if (digit < 0 || digit > 9)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	return (digit);
-}
-
-/**
- * product - multiplies a string of numbers by a single digit.
- * @prod: the buffer to store the result.
- * @mult: the string of numbers.
- * @digit: the single digit.
- * @zeroes: number of leading zeroes.
- *
- * Return: nothing.
- */
-void product(char *prod, char *mult, int digit, int zeroes)
-{
-	int len, num, tens = 0;
-
-	len = _strlen(mult) - 1;
-	mult += len;
-
-	while (*prod)
-	{
-		*prod = 'x';
-		prod++;
-	}
-
-	prod--;
-
-	while (zeroes--)
-	{
-		*prod = '0';
-		prod--;
-	}
-
-	for (; len >= 0; len--, mult--, prod--)
-	{
-		if (*mult < '0' || *mult > '9')
-		{
-			printf("Error\n");
-			exit(98);
-		}
-
-		num = (*mult - '0') * digit;
-		num += tens;
-		*prod = (num % 10) + '0';
-		tens = num / 10;
-	}
-
-	if (tens)
-		*prod = (tens % 10) + '0';
-}
-
-/**
- * add_digits - Adds the numbers stored in two strings.
- * @prod1: The buffer storing the running final product.
- * @prod2: The next product to be added.
- * @len: The length of next_prod.
- */
-void add_digits(char *prod1, char *prod2, int len)
-{
-	int num, tens = 0;
-
-	while (*(prod1 + 1))
-		prod1++;
-
-	while (*(prod2 + 1))
-		prod2++;
-
-	for (; *prod1 != 'x'; prod1--)
-	{
-		num = (*prod1 - '0') + (*prod2 - '0');
-		num += tens;
-		*prod1 = (num % 10) + '0';
-		tens = num / 10;
-
-		prod2--;
-		len--;
-	}
-
-	for (; len >= 0 && *prod2 != 'x'; len--)
-	{
-		num = (*prod2 - '0');
-		num += tens;
-		*prod1 = (num % 10) + '0';
-		tens = num / 10;
-
-		prod1--;
-		prod2--;
-	}
-
-	if (tens)
-		*prod1 = (tens % 10) + '0';
-}
-
-/**
- * main - multiplies two positive numbers.
- * @argc: arguments count.
- * @argv: pointer to the arguments.
- *
- * Return: Always 0.
- */
 int main(int argc, char *argv[])
 {
-	char *prod1, *prod2;
-	int size, i, num, zeroes = 0;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	if (argc != 3)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		handle_error();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		printf("Error\n");
-		exit(98);
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-
-	if (*(argv[1]) == '0')
-		argv[1] = iterate(argv[1]);
-	if (*(argv[2]) == '0')
-		argv[2] = iterate(argv[2]);
-	if (*(argv[1]) == '\0' || *(argv[2]) == '\0')
+	for (i = 0; i < len - 1; i++)
 	{
-		printf("0\n");
-		return (0);
+		if (result[i])
+			a = 1;
+		if (a)
+			putchar(result[i] + '0');
 	}
-
-	size = _strlen(argv[1]) + _strlen(argv[2]);
-	prod1 = init_array(size + 1);
-	prod2 = init_array(size + 1);
-
-	for (i = _strlen(argv[2]) - 1; i >= 0; i--)
-	{
-		num = to_int(*(argv[2] + i));
-		product(prod2, argv[1], num, zeroes++);
-		add_digits(prod1, prod2, size - 1);
-	}
-	for (i = 0; prod1[i]; i++)
-	{
-		if (prod1[i] != 'x')
-			putchar(prod1[i]);
-	}
+	if (!a)
+		putchar('0');
 	putchar('\n');
-
-	free(prod2);
-	free(prod1);
-
+	free(result);
 	return (0);
 }
