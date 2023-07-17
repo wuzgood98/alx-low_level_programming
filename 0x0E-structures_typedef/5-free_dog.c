10c2eba2a06311ccc038eacd6d9c2a33b586d74f
Gideon Addo <gideonaddoofori@gmail.com>
2023-07-17 18:46:53 +0000

    Add 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free dogs.
+ * @d: dog to be free (struct dog).
+ *
+ * Return: nothing.
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
