b042630fb7b4117f8506a682c4105ee8dcc10a52
Gideon Addo <gideonaddoofori@gmail.com>
2023-07-17 14:07:13 +0000

    [Update]: add function description and removed standard library

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+/**
+ * main - prints the name of the file it was compiled from
+ * followed by a new line.
+ *
+ * Return - Always 0.
+ */
 
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	return (EXIT_SUCCESS);
+	return (0);
 }
