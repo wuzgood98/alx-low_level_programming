e8b424a1dd274aafa24a8ec2ead89852f81f01c8
Gideon Addo <gideonaddoofori@gmail.com>
2023-07-17 16:21:09 +0000

    [FIX]: add semi colons at the end of the struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,9 +3,9 @@
 
 /**
  * struct dog - defines the name, owner and age of a dog.
- * @name: name of dog.
- * @owner: the name of the owner of the dog.
+ * @name: name of the dog.
  * @age: age of the dog.
+ * @owner: the name of the owner of the dog.
  *
  * Description: Set the type for the variable 'name' and 'owner' to char
  * and age to float.
@@ -13,8 +13,9 @@
 
 struct dog
 {
-	char *name, *owner;
+	char *name;
 	float age;
-}
+	char *owner;
+};
 
 #endif
