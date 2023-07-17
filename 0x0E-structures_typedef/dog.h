63472e654c9dd243e9be44406b4ccff58f4211d1
Gideon Addo <gideonaddoofori@gmail.com>
2023-07-17 17:13:39 +0000

    [FIX]: documentation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,7 +22,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 /**
- * dog_t - typedef for struct dog
+ * dog_t - Typedef for struct dog
  */
 
 typedef struct dog dog_t;
