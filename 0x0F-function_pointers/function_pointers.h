e1d8ef3c29288581b416760a2bf6bda792b16c88
Gideon Addo <gideonaddoofori@gmail.com>
2023-07-19 12:05:12 +0000

    Add stdlib

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef _function_pointers_
 #define _function_pointers_
 
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
