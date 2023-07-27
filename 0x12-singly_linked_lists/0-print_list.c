ee718d4620c8f5f6dcc13b796d654b35b9af5507
Gideon Addo <gideonaddoofori@gmail.com>
2023-07-27 16:44:20 +0000

    Add singly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of list_t.
+ * @h: pointer to the head of the node.
+ *
+ * Return: the number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/_putchar.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * _putchar - writes the character c to stdout.
+ * @c: the character to print.
+ *
+ * Return: 1 if succesful, otherwise -1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
