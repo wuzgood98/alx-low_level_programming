5cc7ad8dbd0e6da2a086acafd99c24c2623158dd
Gideon Addo <gideonaddoofori@gmail.com>
2023-08-01 13:41:53 +0000

    Add 4-free_list.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list.
+ * @head: pointer to the head of the list.
+ *
+ * Return: nothing.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		/* Store the reference to the next node */
+		current = head->next;
+
+		/* free the memory from the list */
+		free(current);
+		free(head);
+
+		/* Move to the next node */
+		head = current;
+	}
+}
