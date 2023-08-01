69e2f4a8817d4646dbab05976f3bb5996f205d62
Gideon Addo <gideonaddoofori@gmail.com>
2023-08-01 17:14:07 +0000

    Add struct for list pointer

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,19 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct listptr_s - singly linked list
+ * @ptr: pointers of the nodes
+ * @next: points to the next node
+ *
+ * Description: pointers of singly linked list.
+ */
+typedef struct listptr_s
+{
+	void *ptr;
+	struct listptr_s *next;
+} listptr_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
