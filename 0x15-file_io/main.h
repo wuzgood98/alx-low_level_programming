d7b99db6cf1151fc523cd85ffb9969960d6df07b
Gideon Addo <gideonaddoofori@gmail.com>
2023-08-07 16:50:44 +0000

    Add libs

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,11 @@
 #ifndef _main_
 #define _main_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
