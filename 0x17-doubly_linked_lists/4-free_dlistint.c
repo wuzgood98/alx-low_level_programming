d950da57e6ea6c9140521fabd60d520ccb3f0b7b
Gideon Addo <gideonaddoofori@gmail.com>
2023-09-14 12:01:35 +0000

    Completed task 4: Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list.
+ * @head: pointer to the head of the list.
+ *
+ * Return: nothing.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+	}
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp);
+	}
+}
